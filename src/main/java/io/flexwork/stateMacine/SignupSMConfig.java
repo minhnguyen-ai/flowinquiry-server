6fd4e57e5fab407d1dca2e7716bdfd3560b64c1a
Hai Nguyen <haiphucnguyen@gmail.com>
2024-07-03 18:21:22 -0700

    Update

--- src/main/java/io/flexwork/stateMacine/SignupSMConfig.java
+++ src/main/java/io/flexwork/stateMacine/SignupSMConfig.java
@@ -2,9 +2,22 @@ package io.flexwork.stateMacine;
 
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineConfigBuilder;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 @EnableStateMachineFactory
-public class SignupSMConfig extends StateMachineConfigurerAdapter<StateEnum, EventEnum> {}
+public class SignupSMConfig extends StateMachineConfigurerAdapter<StateEnum, EventEnum> {
+
+    @Override
+    public void configure(StateMachineConfigBuilder<StateEnum, EventEnum> config) throws Exception {
+        super.configure(config);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<StateEnum, EventEnum> transitions) throws Exception {
+        super.configure(transitions);
+    }
+}
 
 enum StateEnum {
     A,
