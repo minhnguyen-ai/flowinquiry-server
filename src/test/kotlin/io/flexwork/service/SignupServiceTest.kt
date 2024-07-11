e4e6c7727108d60eec3389d095e08ad50abd5f11
Hai Nguyen <haiphucnguyen@gmail.com>
2024-07-11 10:30:59 -0700

    Update

--- src/test/kotlin/io/flexwork/service/SignupServiceTest.kt
+++ src/test/kotlin/io/flexwork/service/SignupServiceTest.kt
@@ -4,35 +4,35 @@ import io.flexwork.IntegrationTest
 import io.flexwork.domain.User
 import io.flexwork.stateMacine.signup.SignupEvents
 import io.flexwork.stateMacine.signup.SignupStates
+import kotlin.test.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.statemachine.config.StateMachineFactory
 import org.springframework.statemachine.service.StateMachineService
-import kotlin.test.Test
 
 @IntegrationTest
 class SignupServiceTest {
 
-    @Autowired
-    private lateinit var signupService: SignupService
+  @Autowired
+  private lateinit var signupService: SignupService
 
-    @Autowired
-    private lateinit var stateMachineService: StateMachineService<SignupStates, SignupEvents>
+  @Autowired
+  private lateinit var stateMachineService: StateMachineService<SignupStates, SignupEvents>
 
-    private lateinit var stateMachineFactory: StateMachineFactory<SignupStates, SignupEvents>
+  private lateinit var stateMachineFactory: StateMachineFactory<SignupStates, SignupEvents>
 
-    @Test
-    fun signup() {
-        val user = User()
-        user.id = "123"
-        user.login = "hainguyenLogin"
-        user.signupState = SignupStates.NEW_SIGNUP_USER
-        user.email = "test@test.com"
-        signupService.signup(user)
+  @Test
+  fun signup() {
+    val user = User()
+    user.id = "123"
+    user.login = "hainguyenLogin"
+    user.signupState = SignupStates.NEW_SIGNUP_USER
+    user.email = "test@test.com"
+    signupService.signup(user)
 
-        assertThat(user.signupState).isEqualTo(SignupStates.NEW_SIGNUP_USER)
+    assertThat(user.signupState).isEqualTo(SignupStates.NEW_SIGNUP_USER)
 
-        val state = stateMachineService.acquireStateMachine("signu11233p-" + user.id)
-        println("He " + state.state)
-    }
+    val state = stateMachineService.acquireStateMachine("signu11233p-" + user.id)
+    println("He " + state.state)
+  }
 }
