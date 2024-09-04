e4394eb1a24f2e1069575dbbd6274012dd1bbbc7
Hai Nguyen <haiphucnguyen@gmail.com>
2024-09-03 22:13:34 -0700

    Update

--- frontend/src/lib/constants.ts
+++
@@ -1,1 +0,0 @@
-export const BACKEND_API = process.env.BACK_END_SERVER;
--- frontend/src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  console.log("Middleware123 is executed " + request.nextUrl.pathname);
-  if (request.nextUrl.pathname.startsWith("/api")) {
-    console.log("Middleware is executed " + request.nextUrl.pathname);
-  }
-}
