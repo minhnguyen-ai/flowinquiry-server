6a5318d6f42755eb8941e3ef88db6e9df11e0b02
Hai Nguyen <haiphucnguyen@gmail.com>
2024-09-26 22:47:13 -0700

    Update

---
+++ theme.config.jsx
@@ -0,0 +1,26 @@
+export default {
+    logo: <span>Flexwork Documentation</span>,
+    project: {
+      link: 'https://github.com/theflexwork'
+    },
+    
+    head: (
+      <>
+        <link rel="icon" type="image/x-icon" href="favicon/favicon.ico"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Flexwork" />
+        <meta property="og:description" content="The customizable customer service system" />
+      </>
+    ),
+    logo: (
+      <>
+        <img src="/logo.png"/>
+      </>
+    ),
+    editLink: {
+      component: null
+    },
+    feedback: {
+      content: null
+    }
+  }
