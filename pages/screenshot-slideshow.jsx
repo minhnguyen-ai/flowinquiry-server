030fbe81e9539c8084c88e3f02cd6cbe65aafecb
Hai Nguyen <hainguyen@flowinquiry.io>
2025-01-04 22:46:22 -0800

    Optimize images (#16)

--- pages/screenshot-slideshow.jsx
+++ pages/screenshot-slideshow.jsx
@@ -2,6 +2,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import Image from "next/image";
 import React, { useState } from "react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -20,11 +21,11 @@ const ScreenshotSlideshow = ({ slides = [] }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
       {/* Description Section */}
-      <div className="p-6 bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white rounded-lg shadow-lg">
+      <div className="p-8 bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white rounded-lg shadow-lg">
         <h3 className="text-2xl font-bold mb-4">
-          {slides[currentSlide]?.title || "No Title"}
+          {slides[currentSlide]?.title}
         </h3>
         <p className="text-lg">{slides[currentSlide]?.description || ""}</p>
       </div>
@@ -41,11 +42,23 @@ const ScreenshotSlideshow = ({ slides = [] }) => {
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <img
-              src={slide.image}
-              alt={`Screenshot ${index + 1}`}
-              className="w-full rounded-lg shadow-lg"
-            />
+            <div className="flex justify-center items-center h-full">
+              <div className="relative max-w-full">
+                <Image
+                  src={slide.image}
+                  alt={`Screenshot ${index + 1}`}
+                  className="rounded-lg shadow-lg mx-auto"
+                  height={600}
+                  width={500}
+                  loading="lazy"
+                  style={{
+                    display: "block",
+                    margin: "0 auto",
+                    objectFit: "contain",
+                  }}
+                />
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
