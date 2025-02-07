d344e53a39ac3f6d75db64e35e5d41399b4631a1
Minh Nguyen <197183301+minhnguyen-ai@users.noreply.github.com>
2025-02-07 15:14:05 -0800

    Added workflow service integration tests (#66)

---
+++ commons/src/main/java/io/flowinquiry/modules/audit/AuditEntityDTO.java
@@ -0,0 +1,22 @@
+package io.flowinquiry.modules.audit;
+
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class AuditEntityDTO {
+    private Long createdBy;
+    private String createdByUserName; // Fetching username instead of entity reference
+    private Instant createdAt;
+    private Long modifiedBy;
+    private String modifiedByUserName; // Fetching username instead of entity reference
+    private Instant modifiedAt;
+}
