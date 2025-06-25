440f1f1d0b11f2e2206bad40aa92eee8002678a0
Hai Nguyen <3423575+haiphucnguyen@users.noreply.github.com>
2025-06-25 11:20:44 -0700

    Update spring ai dependencies (#234)

--- apps/backend/commons/src/main/java/io/flowinquiry/modules/ai/service/OllamaChatModelService.java
+++ apps/backend/commons/src/main/java/io/flowinquiry/modules/ai/service/OllamaChatModelService.java
@@ -1,6 +1,7 @@
 package io.flowinquiry.modules.ai.service;
 
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
         name = {"OLLAMA_CHAT_MODEL", "OLLAMA_API_KEY"},
         matchIfMissing = false)
 public class OllamaChatModelService implements ChatModelService {
+
+    private OllamaChatModel olamaChatModel;
 
     @Override
     public String call(String input) {
