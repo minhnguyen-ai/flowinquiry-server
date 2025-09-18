1cc91fb13e653371361a249850ae91ebadc21c76
Biswarup Chatterjee <biswarup.chatterjee@tuta.io>
2025-09-19 00:10:01 +0530

    Add sprint itreation name generator service (#257)

---
+++ apps/backend/server/src/test/java/io/flowinquiry/modules/teams/utils/ProjectIterationNameGeneratorTest.java
@@ -0,0 +1,34 @@
+package io.flowinquiry.modules.teams.utils;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProjectIterationNameGeneratorTest {
+    @Test
+    public  void testGetNextIterationWithValidData(){
+        //Given
+        String currentIterationName1 = "Sprint 1";
+        String currentIterationName2 = "Iteration 2024-W20";
+        String currentIterationName3 = "Sprint";
+        String currentIterationName4 = "Sprin1t";
+        String currentIterationName5 = "Sprint9";
+
+        //When
+        String nextIterationName1 = ProjectIterationNameGenerator.getNextIteration(currentIterationName1);
+        String nextIterationName2 = ProjectIterationNameGenerator.getNextIteration(currentIterationName2);
+        String nextIterationName3 = ProjectIterationNameGenerator.getNextIteration(currentIterationName3);
+        String nextIterationName4 = ProjectIterationNameGenerator.getNextIteration(currentIterationName4);
+        String nextIterationName5 = ProjectIterationNameGenerator.getNextIteration(currentIterationName5);
+
+
+
+        //Then
+        assertEquals("Sprint 2",nextIterationName1);
+        assertEquals("Iteration 2024-W21",nextIterationName2);
+        assertEquals("Sprint 1",nextIterationName3);
+        assertEquals("Sprin1t 1",nextIterationName4);
+        assertEquals("Sprint10",nextIterationName5);
+    }
+
+}
